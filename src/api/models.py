7f42ecdadce20d931f50240826508221831b23e9
Carlos Corona Pestana <136831607+carloscopes@users.noreply.github.com>
2023-10-13 18:08:34 +0000

    Endpoint con creación de token

--- src/api/models.py
+++ src/api/models.py
@@ -5,8 +5,8 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    password = db.Column(db.String(180), unique=False, nullable=False)
+    fullname = db.Column(db.String(120), nullable=False)
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -15,5 +15,6 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "fullname": self.fullname
             # do not serialize the password, its a security breach
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -4,15 +4,39 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import create_access_token
 
 api = Blueprint('api', __name__)
 
+@api.route('/signup', methods=["POST"])
+def sign_up():
+    body = request.json
+    #  Validar si enviaron email, password y fullname
+    user = User(email=body.get("email"), password = body.get("password"), fullname = body.get("fullname"))
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+    try:
+        db.session.add(user)
+        db.session.commit()
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+        return jsonify({"message": "Usuario registrado exitosamente"})
 
-    return jsonify(response_body), 200
+    except Exception as error:
+        print(error)
+
+    return jsonify([]), 200
+
+@api.route('/login', methods=["POST"])
+def login():
+    body = request.json
+    email = body.get("email", None)
+    password = body.get("password", None)
+    user = User.query.filter_by(email=email).one_or_none()
+
+    if user is None:
+        return jsonify({ "msg": "Bad credencials" }), 400
+    else:
+        access_token = create_access_token(identity=user.id)
+        return jsonify({
+            "token": access_token,
+            "user_id": user.id
+        }), 200
