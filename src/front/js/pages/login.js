3f0f21004303656d0e0bfbd1dd70aef10ef5b7e0
Carlos Corona Pestana <136831607+carloscopes@users.noreply.github.com>
2023-10-14 16:09:28 +0000

    Autenticación de login con token en LocalStorage

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -1,12 +1,25 @@
 import React, { useState, useContext, useEffect} from "react";
 import { Context } from "../store/appContext";
+import { useNavigate } from "react-router-dom";
+
 
 export const LogIn = () => {
-   
+
+    const {actions, store} = useContext(Context)
+
+   const [email, setEmail] = useState("")
+   const [password, setPassword] = useState("")
+   const navigate = useNavigate()
+
+   useEffect(() => {
+    if (store.token) {
+        navigate("/private")
+    }
+   }, [store.token])
 
     return (
-        <main className="login">
-            <form
+        <>
+            <div
                 className="login__form"
                 >
                 <div className="form__contenedor">
@@ -17,6 +30,7 @@ export const LogIn = () => {
                         className="form__contenedor__items form__input"
                         type="email"
                         id="email"
+                        onChange={event => setEmail(event.target.value)}
                         placeholder="Email"
                         required
                     />
@@ -25,20 +39,21 @@ export const LogIn = () => {
                     <label
                         className="form__contenedor__items"
                         htmlFor="contraseña">
-                        Contraseña
+                        Contraseña {password}
                     </label>
                     <input
                         className="form__contenedor__items form__input"
                         type="password"
+                        onChange={event => setPassword(event.target.value)}
                         id="contraseña"
                         placeholder="Contraseña"
                         required
                     />
                 </div>
-                <button type="submit" className="submit">
+                <button onClick={event => actions.login(email, password)}>
                     Iniciar sesión
                 </button>
-            </form>
-        </main>
+            </div>
+        </>
     );
 };
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,26 +1,9 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			token: localStorage.getItem("session-token")
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-
 			getMessage: async () => {
 				try{
 					// fetching data from the backend
@@ -33,20 +16,27 @@ const getState = ({ getStore, getActions, setStore }) => {
 					console.log("Error loading message from backend", error)
 				}
 			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+			
+			login: async (email, password) => {
+				try{
+					const response = await fetch(process.env.BACKEND_URL + 'api/login', {
+						method: "POST", 
+						body: JSON.stringify({email, password}),
+						headers: {
+						  "Content-Type": "application/json",
+						},
+					  })
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
+					  const data = await response.json()
+					  localStorage.setItem("session-token", data.token)
+					  setStore({token: data.token})
+					  
+				
+				}catch(error){
+					console.log("Error loading message from backend", error)
+				}
+			},
+	
 		}
 	};
 };
