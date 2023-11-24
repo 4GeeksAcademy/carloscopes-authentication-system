8a0a1ee0e25b4102d19bdc81d4e035177142b32e
Carlos Corona Pestana <136831607+carloscopes@users.noreply.github.com>
2023-11-24 18:45:21 +0000

    Definicion del constructor en User model

--- src/api/models.py
+++ src/api/models.py
@@ -9,7 +9,12 @@ class User(db.Model):
     fullname = db.Column(db.String(120), nullable=False)
 
     def __repr__(self):
-        return f'<User {self.email}>'
+        return f'<User {self.fullname} {self.email}>'
+
+    def __init__(self, email, password, fullname):
+        self.email = email
+        self.password = password
+        self.fullname = fullname
 
     def serialize(self):
         return {
